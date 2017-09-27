d0898d961bacc24f0a6a5ab7c7007104d4b88b49
hmngwy <hmngwy@users.noreply.github.com>
2017-09-27 23:24:54 +0800

    fixed cache expiry check

--- scripts/weatheralert/weather_local.sh
+++ scripts/weatheralert/weather_local.sh
@@ -1,15 +1,8 @@
 run_segment() {
   FILE=/tmp/weather_local_cache
-  OLDTIME=300
-  CURTIME=$(date +%s)
-  FILETIME=$(stat $FILE -c %Y)
-  TIMEDIFF=$(expr $CURTIME - $FILETIME)
-  # only fetch if our cache is old
-  if [ $TIMEDIFF -gt $OLDTIME ] || [ ! -f $FILE ]; then
-    echo "File is older, do stuff here"
-    $WORKON_HOME/.dotfiles/bin/python $HOME/.dotfiles/scripts/weatheralert/index.py "Marikina, Metro Manila" $FILE
+  if test "`find $FILE -mmin +5`"; then
+    $WORKON_HOME/.dotfiles/bin/python $HOME/.dotfiles/scripts/weatheralert/index.py "Makati, Metro Manila" $FILE
   fi
   cat $FILE
-	return 0
+  return 0
 }
-
