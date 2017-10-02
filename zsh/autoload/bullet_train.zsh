59cbb7ffed7dcbac512cfe3ac560aea515f4194b
Pat Ambrosio <cp.ambrosio@gmail.com>
2017-10-02 10:09:50 +0800

    made autoload dir

--- zsh/autoload/bullet_train.zsh
+++ zsh/autoload/bullet_train.zsh
@@ -1,15 +1,3 @@
-gpip(){
-   PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
-}
-
-publish () {
-  mv $1 "$(date +%Y-%m-%d) $1"
-}
-
-edit () {
-  editor $(ls | grep $1)
-}
-
 # Bullet Train Theme Customization
 BULLETTRAIN_TIME_SHOW=false
 BULLETTRAIN_DIR_FG='black'
@@ -76,17 +64,3 @@ BULLETTRAIN_PROMPT_ORDER=(
   dirvenv
   git
 )
-
-setopt no_complete_aliases
-
-alias tmux='tmux -2'
-alias reload='source ~/.zshrc'
-alias fu=cmdfu
-alias show_palette='for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done'
-alias wan='curl ipinfo.io/ip'
-alias irssi='TERM=screen-256color irssi'
-alias vim=nvim
-alias cat=ccat
-alias h='howdoi -n 3'
-alias gs=gst
-
