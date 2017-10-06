a84dafa86e862d744048f76d4e733bfc82eb3a36
Pat Ambrosio <cp.ambrosio@gmail.com>
2017-10-06 13:49:42 +0800

    fix weather segment

--- python/scripts/weatheralert/weather_local.sh
+++ python/scripts/weatheralert/weather_local.sh
@@ -1,7 +1,7 @@
 FILE=/tmp/weather_local_cache
 if [ ! -f $FILE ] || test "`find $FILE -mmin +5`"; then
-  $WORKON_HOME/.dotfiles/bin/python $HOME/.dotfiles/python/scripts/weatheralert/index.py "Makati, Metro Manila" 10 $FILE
+  $WORKON_HOME/.dotfiles/bin/python $HOME/.dotfiles/python/scripts/weatheralert/index.py "Makati, Metro Manila" 30 $FILE
 fi
 output=$(cat $FILE)
 
-[[ "${output// }" ]] && echo "$output"
+[[ "${output// }" ]] && echo "#[fg=colour15,bg=colour88,bold] $output"
