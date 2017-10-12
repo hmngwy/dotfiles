0d4ba2da5b0075847ee440e464c7fbf2c5fb50d8
Pat Ambrosio <cp.ambrosio@gmail.com>
2017-10-12 16:12:42 +0800

    Adds exit code to RPROMPT

--- zsh/autoload/bullet_train.zsh
+++ zsh/autoload/bullet_train.zsh
@@ -64,3 +64,14 @@ BULLETTRAIN_PROMPT_ORDER=(
   dirvenv
   git
 )
+
+function check_last_exit_code() {
+  local LAST_EXIT_CODE=$?
+  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+    local EXIT_CODE_PROMPT=' '
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE%{$reset_color%}"
+    echo "$EXIT_CODE_PROMPT"
+  fi
+}
+
+RPROMPT='$(check_last_exit_code)'
