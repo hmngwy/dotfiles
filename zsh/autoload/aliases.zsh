e697e3f81145cc139c6a3b6b9d020048f91c0ff6
hmngwy <cp.ambrosio@gmail.com>
2017-10-17 19:51:24 +0800

    renamed show_pallete

--- zsh/autoload/aliases.zsh
+++ zsh/autoload/aliases.zsh
@@ -15,7 +15,7 @@ edit () {
 alias tmux='tmux -2'
 alias reload='source ~/.zshrc'
 alias fu=cmdfu
-alias show_palette='for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done'
+alias termcolors='for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done'
 alias wan='curl ipinfo.io/ip'
 alias irssi='TERM=screen-256color irssi'
 alias vim=nvim
