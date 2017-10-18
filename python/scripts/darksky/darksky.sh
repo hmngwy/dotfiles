eb7af527312d7177d39d55db965b68d0ea35b793
hmngwy <hmngwy@users.noreply.github.com>
2017-10-18 21:38:36 +0800

    moved darksky to tpm plugin

--- python/scripts/darksky/darksky.sh
+++
@@ -1,6 +0,0 @@
-FILE=/tmp/darksky_cache
-if [ ! -f $FILE ] || test "`find $FILE -mmin +10`"; then
-  $WORKON_HOME/.dotfiles/bin/python $HOME/.dotfiles/python/scripts/darksky/index.py "14.5833,120.9667" > $FILE
-fi
-output=$(cat $FILE)
-echo $output
